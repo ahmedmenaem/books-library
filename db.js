b7d722e7701c95ffd6dbf821ffed3ab714b5aa55
Ahmed Menaem <ahmed.menaem@almtar.local>
2019-02-13 13:31:36 +0200

    Add database connect function.

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const config = require("./config/dev");
+
+const connect = (url = config.dbUrl, opts = {}) => {
+  return mongoose.connect(url, { ...opts, useNewUrlParser: true });
+};
+
+module.exports = {
+  connect
+};
